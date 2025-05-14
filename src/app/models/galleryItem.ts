2140cbcfb6154403cdc65b632d1abe9344a6cea7
edvardakerman <ed@sagovagen.se>
2025-05-14 14:23:46 +0200

    svp assignment bug fix

--- src/app/models/galleryItem.ts
+++ src/app/models/galleryItem.ts
@@ -2,11 +2,7 @@ export interface GalleryItem {
     title: string;
     desc: string;
     image: string;
-    link: string;
     type: string;
-    btnStyle: string;
-    btnText: string;
-    btnIcon?: string;
     extraBtn?: {
         text: string;
         link: string;
